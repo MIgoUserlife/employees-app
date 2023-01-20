bd8c953878129317c8f06bcfd7405c5d8f88de97
Laskarzhevskyi Mykhailo <migo.userlife@gmail.com>
2023-01-20 09:59:01 +0200

    include to views adminlte template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,1 @@
-@extends('adminlte::page')
-
-@section('title', 'Dashboard')
-
-@section('content_header')
-    <h1>Dashboard</h1>
-@stop
-
-@section('content')
-    <p>Welcome to this beautiful admin panel.</p>
-@stop
+@extends('layouts.app')
